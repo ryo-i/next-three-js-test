b768be6cc1fabab656818aff7f94e3d4b36ed06a
ryo-i <iidazz0109@gmail.com>
2022-12-22 05:32:06 +0900

    DodecahedronGeometryで十二面体を作る #8

--- pages/about.tsx
+++ pages/about.tsx
@@ -58,7 +58,7 @@ function About() {
             <section>
                 <h2>進捗</h2>
                 <ul>
-                    <li>CylinderGeometryで円筒を作る <a href="https://github.com/ryo-i/next-app-started/issues/8">#8</a></li>
+                    <li>DodecahedronGeometryで十二面体を作る <a href="https://github.com/ryo-i/next-app-started/issues/8">#8</a></li>
                     <li>テキストやurlの共通部分をidに統合 <a href="https://github.com/ryo-i/next-app-started/issues/3">#3</a></li>
 
                 </ul>
