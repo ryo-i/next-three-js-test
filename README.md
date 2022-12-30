66bc1de11094fcb484d7268beeb5c0d3e748e9f3
ryo-i <iidazz0109@gmail.com>
2022-12-30 09:58:40 +0900

    ろくろ回し的な形状(LatheGeometry) #8

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Next.js環境でthree.jsが動くかテスト
 
 ## 進捗（コミット）
 
-- DodecahedronGeometryで十二面体を作る [#8](https://github.com/ryo-i/next-three-js-test/issues/8)
+- ろくろ回し的な形状(LatheGeometry) [#8](https://github.com/ryo-i/next-three-js-test/issues/8)
 - テキストやurlの共通部分をidに統合 [#3](https://github.com/ryo-i/next-three-js-test/issues/3)
 
 ## 詳細（ブログ）
--- pages/about.tsx
+++ pages/about.tsx
@@ -58,7 +58,7 @@ function About() {
             <section>
                 <h2>進捗</h2>
                 <ul>
-                    <li>DodecahedronGeometryで十二面体を作る <a href="https://github.com/ryo-i/next-app-started/issues/8">#8</a></li>
+                    <li>ろくろ回し的な形状(LatheGeometry) <a href="https://github.com/ryo-i/next-app-started/issues/8">#8</a></li>
                     <li>テキストやurlの共通部分をidに統合 <a href="https://github.com/ryo-i/next-app-started/issues/3">#3</a></li>
 
                 </ul>
