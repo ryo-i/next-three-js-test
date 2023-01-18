a563b2c4c993b827fc00a65c090a6f8eb8da135f
ryo-i <iidazz0109@gmail.com>
2023-01-19 06:53:20 +0900

    TorusGeometryで円環（ドーナツ）を作る #8

---
+++ components/Inner_024.tsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect, useRef }  from 'react';
+import styled from 'styled-components';
+import * as THREE from 'three/src/Three';
+
+// CSS in JS
+const Figure = styled.figure`
+  canvas {
+    box-shadow: 0 0 15px 2px rgb(0 0 0 / 10%);
+  }
+`;
+
+// Component
+function Inner() {
+  const [canvasSize, setCanvasSize] = useState(0);
+  const figureElm = useRef(null);
+
+  const changeCanvasSize = (canvasElmWidth) => {
+    if (canvasElmWidth < 900) {
+      setCanvasSize(canvasElmWidth);
+    } else {
+      setCanvasSize(900);
+    }
+  }
+
+  useEffect(() => {
+    const canvasElmWidth = figureElm.current.clientWidth;
+    // console.log('canvasElmWidth(load)', canvasElmWidth);
+    changeCanvasSize(canvasElmWidth);
+
+    window.addEventListener('resize', () => {
+      const canvasElmWidth = figureElm.current.clientWidth;
+      // console.log('canvasElmWidth(resize)', canvasElmWidth);
+      changeCanvasSize(canvasElmWidth);
+    });
+  }, [0]);
+
+
+  useEffect(() => {
+    if (!canvasSize) {
+      return;
+    }
+
+    // three.js
+    const scene = new THREE.Scene();
+
+    const fov = 35;
+    const aspect = canvasSize / canvasSize;
+    const near = 0.1;
+    const far = 1000;
+    const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.z = 80;
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( canvasSize, canvasSize );
+    if (figureElm.current.firstChild) {
+      figureElm.current.removeChild( figureElm.current.firstChild );
+    }
+    figureElm.current.appendChild( renderer.domElement );
+
+    const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+    scene.add( light );
+
+    const x = -2.5;
+    const y = -5;
+
+    // Basic
+    const geometry1 = new THREE.TorusGeometry(
+      5, // ui: radius,
+      2, // ui: tubeRadius,
+      8, // ui: radialSegments,
+      12 // ui: tubularSegments
+    );
+
+
+    // Expansion
+    const geometry2 = new THREE.TorusGeometry(
+      5, // ui: radius,
+      2, // ui: tubeRadius,
+      80, // ui: radialSegments,
+      120, // ui: tubularSegments
+      Math.PI * 2 // ui: arc
+    );
+
+
+    // Custom
+    const geometry3 = new THREE.TorusGeometry(
+      5, // ui: radius,
+      2, // ui: tubeRadius,
+      3, // ui: radialSegments,
+      4, // ui: tubularSegments
+      Math.PI * 1.2 // ui: arc
+    );
+
+
+    function makeInstance(geometry, color, x) {
+      const material = new THREE.MeshPhongMaterial({color});
+      const primitive = new THREE.Mesh(geometry, material);
+
+      scene.add(primitive);
+      primitive.position.x = x;
+
+      return primitive;
+    }
+
+    const primitives = [
+      makeInstance(geometry1, 'green',  -15),
+      makeInstance(geometry2, 'yellow', 0),
+      makeInstance(geometry3, 'red',  15),
+    ];
+
+    function render(time) {
+      time *= 0.0005;  // convert time to seconds
+
+      primitives.forEach((primitive, ndx) => {
+        const speed = 1 + ndx * .1;
+        const rot = time * speed;
+        primitive.rotation.x = rot;
+        primitive.rotation.y = rot;
+      });
+
+      renderer.render(scene, camera);
+
+      requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+    console.log('canvasSize', canvasSize)
+  }, [canvasSize]);
+
+
+  // JSX
+  return (
+    <Figure ref={figureElm}></Figure>
+  );
+}
+
+export default Inner;
